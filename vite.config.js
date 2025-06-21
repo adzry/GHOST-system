b8717b18be13d6037e1d7d00f9501b0565e6fe9e
adzry <adzrysapie@email.com>
2025-06-21 15:00:28 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/GHOST-system/', // <<< Must match your repo name!
+  base: '/GHOST-system/',
   plugins: [react()],
 })
